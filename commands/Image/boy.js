85ad71a911a2a51b342fb15d9a14efa4d0213f95
Nguyễn Hồng Đức <contact.hongduc@gmail.com>
2021-10-11 20:54:17 +0700

    Update boy.js

--- commands/Image/boy.js
+++ commands/Image/boy.js
@@ -12,6 +12,8 @@ module.exports = {
             let boyEmbed = new MessageEmbed()
                 .setColor('GREEN')
                 .setAuthor(`Boy`, message.guild.iconURL)
+                .setURL(data.url)
+                .setTitle('Download here')
                 .setImage(data.url)
                 .setTimestamp()
                 .setFooter(`Bot: Duwc510_ by hongduccodedao`, client.user.displayAvatarURL)
