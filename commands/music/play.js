f4c2c62e1ee7094efe95ce72869da4b437bac3c9
Nguyễn Hồng Đức <contact.hongduc@gmail.com>
2021-10-13 22:16:19 +0700

    Update play.js

--- commands/music/play.js
+++ commands/music/play.js
@@ -1,11 +1,25 @@
-// const  { checkSameRoom } = require('../../utils')
-// module.exports = {
-//     name: 'play',
-//     category: 'music',
-//     aliases: ['p'],
-//     description: 'Chơi nhạc từ youtube, spotify, soundcloud',
-//     run: async(client, message, args) => {
-//         if (checkSameRoom(message)) return;
-//         await client.player.play (message, args, join(' '),true);
-//     }
-// }
+const  { checkSameRoom } = require('../../utils')
+module.exports = {
+    name: 'play',
+    category: 'music',
+    aliases: ['p'],
+    description: 'Chơi nhạc từ youtube, spotify, soundcloud',
+    run: async(client, message, args) => {
+        if (checkSameRoom(message)) return;
+        const query = args.join(' ');
+        const queue = client.player.createQueue(message.guild, {
+            metadata: message,
+        });
+        const song = await client.player.search(query, {
+          requestedBy: message.author
+        });
+         
+        try {
+            await queue.connect(message.member.voice.channel);
+        } catch {
+            message.reply("Could not join your voice channel");
+        }
+        
+        queue.play(song.tracks[0]);
+    }
+}
