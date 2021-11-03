100798e176a39437105608f1064bab33ce2ea842
Nguyễn Hồng Đức <contact.hongduc@gmail.com>
2021-11-03 20:43:04 +0700

    init

---
+++ commands/fun/github.js
@@ -0,0 +1,22 @@
+const {MessageEmbed} = require('discord.js');
+const fetch = require('node-fetch');
+module.exports = {
+    name: 'github',
+    category: 'fun',
+    run: async (client, message, args) => {
+        let msg = await message.channel.send('Vui lòng chờ...');
+        
+        let body = await fetch (`https://api.popcat.xyz/github/${encodeURIComponent(args.join(' '))}`)
+        const data = await body.json()
+        if (!data) return message.channel.send('Lỗi vui lòng thử lại sau');
+            let gEmbed = new MessageEmbed()
+                .setColor('GREEN')
+                .setAuthor(`🚩Infomation about Github account`)
+                .setTitle(`**৹ Name**: ${data.name}`)
+                .setImage(data.avatar)
+                .setDescription(`**Bio:** ${data.bio}\n**Account type:** ${data.account_type}\n **Company:** ${data.company}\n**Blog:** ${data.blog}\n**Location:** ${data.location}\n**Email:** ${data.email}\n**Twitter:** ${data.twitter}\n**Public repositories:** ${data.public_repos}\n**Followers:** ${data.followers}\n**Following:** ${data.following}`)
+                .setFooter(`Bot: Duwc510_ by hongduccodedao`, client.user.displayAvatarURL)
+
+        msg.edit({ content:'\u200b',embeds: [gEmbed] });
+    }
+}
