b5a62ce8e927e3d067364b8a47c958a2725f77fe
Nguyễn Hồng Đức <contact.hongduc@gmail.com>
2021-11-07 14:51:17 +0700

    Update github.js

--- commands/fun/github.js
+++ commands/fun/github.js
@@ -4,11 +4,17 @@ module.exports = {
     name: 'github',
     category: 'fun',
     run: async (client, message, args) => {
-        let msg = await message.channel.send('Vui lòng chờ...');
+        let searchEmbed = new MessageEmbed()
+        .setColor('GREEN')
+        .setAuthor('Đang tìm kiếm, vui lòng đợi...', 'https://cdn.discordapp.com/emojis/892292745916481556.gif?size=128')
+        let searching = await message.channel.send({embeds: [searchEmbed]})
         
         let body = await fetch (`https://api.github.com/users/${encodeURIComponent(args.join(' '))}`)
         const data = await body.json()
-        if (!data) return message.channel.send('Lỗi vui lòng thử lại sau');
+        const noData = new MessageEmbed()
+        .setColor('RED')
+        .setDescription(`Có lỗi xảy ra trong quá trình thực hiện!`)
+        if(!data) return searching.edit({embeds : [noData]})
             let gEmbed = new MessageEmbed()
                 .setColor('GREEN')
                 .setAuthor(`🚩Infomation about Github account`)
@@ -17,6 +23,6 @@ module.exports = {
                 .setDescription(`**Bio:** ${data.bio}\n**Account type:** ${data.account_type}\n **Company:** ${data.company}\n**Blog:** ${data.blog}\n**Location:** ${data.location}\n**Email:** ${data.email}\n**Twitter:** ${data.twitter}\n**Public repositories:** ${data.public_repos}\n**Followers:** ${data.followers}\n**Following:** ${data.following}`)
                 .setFooter(`Bot: Duwc510_ by hongduccodedao`, client.user.displayAvatarURL)
 
-        msg.edit({ content:'\u200b',embeds: [gEmbed] });
+                return searching.edit({embeds: [gEmbed]})
     }
 }
