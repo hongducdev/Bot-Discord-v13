2a8cabca31d839208c87df7dbf8e824d2aa1db52
Nguyễn Hồng Đức <contact.hongduc@gmail.com>
2021-11-07 18:53:06 +0700

    Update stop.js

--- commands/music/stop.js
+++ commands/music/stop.js
@@ -1,4 +1,6 @@
 const  { checkSameRoom, noMusicEmbed } = require('../../utils')
+const { MessageEmbed } = require('discord.js');
+
 module.exports = {
     name: 'stop',
     category: 'music',
@@ -7,6 +9,13 @@ module.exports = {
     run: async(client, message, args) => {
         if (checkSameRoom(message)) return;
         const queue = client.player.getQueue(message.guild.id);
-        if (queue) queue.destroy();
+        const stopEmbed = new MessageEmbed()
+            .setColor ('GREEN')
+            .setDescription(`**Đặc Vụ Con Mèo đi đây!**`)
+        if (queue) {
+            message.reply({embeds  : [stopEmbed]});
+            queue.destroy();
+        }
+        
     }
 }
