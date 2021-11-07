cb142361b4765cc2919e7a024c9408070209e664
Nguyễn Hồng Đức <contact.hongduc@gmail.com>
2021-11-07 12:22:33 +0700

    Add files via upload

---
+++ commands/Image/car.js
@@ -0,0 +1,36 @@
+const {MessageEmbed} = require('discord.js');
+const fetch = require('node-fetch');
+module.exports = {
+    name: 'car',
+    category: 'Image',
+    aliases: ['xe'],
+    utilisation: '{prefix}xe',
+    run: async (client, message, args) => {
+        let searchEmbed = new MessageEmbed()
+        .setColor('GREEN')
+        .setAuthor('Đang tìm kiếm, vui lòng đợi...', 'https://cdn.discordapp.com/emojis/892292745916481556.gif?size=128')
+        let searching = await message.channel.send({embeds: [searchEmbed]})
+        const url = await fetch(`https://api.popcat.xyz/car`)
+        const data = await url.json()
+        .then(data=> {
+            //không trả về thì gửi về embed lỗi
+            const noData = new MessageEmbed()
+            .setColor('RED')
+            .setDescription(`Có lỗi xảy ra trong quá trình tìm ảnh!`)
+            if(!data) return searching.edit({embeds : [noData]})
+
+            //nếu có thì trả về ảnh
+            const imageEmbed = new MessageEmbed()
+            .setColor('GREEN')
+            .setAuthor('Image: Car','https://cdn.discordapp.com/emojis/892292745916481556.gif?size=128')
+            .setDescription(`**Information**: ${data.title}`)
+            .setURL(data.image)
+            .setTitle('Download here')
+            .setImage(data.image)
+            .setTimestamp()
+            .setFooter(`Bot: Đặc Vụ Con Mèo by hongduccodedao`, client.user.displayAvatarURL)
+            
+            return searching.edit({embeds: [imageEmbed]})
+        })
+    }
+}
---
+++ commands/Image/girl.js
@@ -0,0 +1,35 @@
+const {MessageEmbed} = require('discord.js');
+const fetch = require('node-fetch');
+module.exports = {
+    name: 'girl',
+    category: 'Image',
+    aliases: ['gai'],
+    utilisation: '{prefix}gai',
+    run: async (client, message, args) => {
+        let searchEmbed = new MessageEmbed()
+        .setColor('GREEN')
+        .setAuthor('Đang tìm kiếm, vui lòng đợi...', 'https://cdn.discordapp.com/emojis/892292745916481556.gif?size=128')
+        let searching = await message.channel.send({embeds: [searchEmbed]})
+        const url = await fetch(`https://api.huyapi.ga/v2/?type=gai`)
+        const data = await url.json()
+        .then(data=> {
+            //không trả về thì gửi về embed lỗi
+            const noData = new MessageEmbed()
+            .setColor('RED')
+            .setDescription(`Có lỗi xảy ra trong quá trình tìm ảnh!`)
+            if(!data) return searching.edit({embeds : [noData]})
+
+            //nếu có thì trả về ảnh
+            const imageEmbed = new MessageEmbed()
+            .setColor('GREEN')
+            .setAuthor('Image: Girl','https://cdn.discordapp.com/emojis/892292745916481556.gif?size=128')
+            .setURL(data.url)
+            .setTitle('Download here')
+            .setImage(data.url)
+            .setTimestamp()
+            .setFooter(`Bot: Đặc Vụ Con Mèo by hongduccodedao`, client.user.displayAvatarURL)
+            
+            return searching.edit({embeds: [imageEmbed]})
+        })
+    }
+}
