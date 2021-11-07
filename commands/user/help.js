1adb93a4ae513dc404480772851e12a70cc651a4
Nguyễn Hồng Đức <contact.hongduc@gmail.com>
2021-11-07 19:28:40 +0700

    Update help.js

--- commands/user/help.js
+++ commands/user/help.js
@@ -5,7 +5,7 @@ module.exports = {
     name: 'help',
     category: 'user',
     description: 'Hưỡng dẫn cách xài lệnh',
-    usage: 'l!help [Tên lệnh]',
+    usage: '%help [Tên lệnh]',
     run: async(client, message, args) => {
         if (!args[0]) return getAll(client, message);
     },
@@ -14,7 +14,10 @@ module.exports = {
 function getAll(client, message) {
     const embed = new MessageEmbed()
         .setColor ('GREEN')
-        .setAuthor('Sử dụng %help để xem hưỡng dẫn sử dụng bot Duwc510._')
+        .setAuthor('Lệnh của Đặc Vụ Con Mèo!', 'https://cdn.discordapp.com/avatars/809665557595029514/26d6026a8999165d391ad7c65698d556.png?size=100')
+        .setThumbnail(`**Prefix: % + Lệnh bạn muốn dùng.**`)
+        .setTimestamp()
+        .setFooter(`Bot: Đặc Vụ Con Mèo by hongduccodedao`, client.user.displayAvatarURL)
 
     const commands = (category) => {
         return client.commands
