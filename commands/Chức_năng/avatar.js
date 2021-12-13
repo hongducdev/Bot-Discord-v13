76c2d1da42dedd4f3f22552fdd20bf908f1acd87
Nguyễn Hồng Đức <contact.hongduc@gmail.com>
2021-12-13 20:22:34 +0700

    Update avatar.js

--- commands/Chức_năng/avatar.js
+++ commands/Chức_năng/avatar.js
@@ -1,4 +1,4 @@
-const {MessageEmbed} = require('discord.js');
+const { MessageEmbed } = require('discord.js');
 
 module.exports = {
     name: 'avatar',
@@ -7,15 +7,18 @@ module.exports = {
     utilisation: '{prefix}avatar',
     run (client, message, args) {
         const member = message.mentions.members.first()|| message.guild.members.cache.get(args[0]) || message.member
-            const URL = member.user.avatarURL({format: 'jpg', dynamic: true, size: 1024})
-            const avatarEmbed = new MessageEmbed()
-                .setAuthor('Avatar Discord', client.user.displayAvatarURL({ size: 1024, dynamic: true }))
-                .setColor ('GREEN')
-                .setImage(URL)
-                .setURL(URL)
-                .setTitle('Tải xuống tại đây')
-                .setTimestamp()
-                .setFooter(`Bot: Bao-Chan Bot by PinkDuwc._`, message.author.avatarURL({ dynamic: true }))
-            message.channel.send({ embeds: [avatarEmbed] });
+        const serverAvatar = member.avatar && `https://cdn.discordapp.com/guilds/${message.guild.id}/users/${member.id}/avatars/${member.avatar}.png?size=512`
+
+        const embed = new MessageEmbed()
+        .setAuthor('Bao-Chan Bot', client.user.displayAvatarURL({ size: 1024, dynamic: true }))
+        .setTitle(`${member.displayName}'s Avatar`)
+        .setImage(serverAvatar || member.user.displayAvatarURL({ dynamic: true, size: 512 }))
+        .setFooter(message.member.displayName,  message.author.displayAvatarURL({ dynamic: true }))
+        .setTimestamp()
+        .setColor(member.displayHexColor);
+        if (serverAvatar) {
+        embed.setThumbnail(member.user.displayAvatarURL({ dynamic: true, size: 512 }))
+        }
+        message.channel.send({embeds: [embed]});
     }
 }
